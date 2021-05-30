4b0cfc5f5bfc9cb9ce8184fbc6f757f1a779c2c2
justochacon <justochacon18@gmail.com>
2021-05-30 17:44:05 -0500

    ms-demo-consultar v1.0

--- src/main/java/ms/prueba/consultar/controller/ConsultarController.java
+++ src/main/java/ms/prueba/consultar/controller/ConsultarController.java
@@ -3,6 +3,7 @@ package ms.prueba.consultar.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,24 +13,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import ms.prueba.consultar.config.Configuracion;
+import ms.prueba.consultar.model.Productos;
+import ms.prueba.consultar.model.RequestService;
+import ms.prueba.consultar.model.RequestServiceReclamo;
+import ms.prueba.consultar.model.ResponseServicio;
+import ms.prueba.consultar.model.ResponseServicioReclamo;
+
 @RestController
 @RequestMapping("/demo/v1")
 public class ConsultarController {
-
+    
+    @Autowired
+    private Configuracion properties;
+    
     @GetMapping(value = "/consultar", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
     public ResponseServicio consultar(RequestService request) {
 
         ResponseServicio response = new ResponseServicio();
         Productos producto = new Productos();
-        producto.setCodProducto("PRO0001");
+        producto.setCodProducto(properties.getPrefijcodigoProducto() + "0001");
         producto.setNomProducto("TV");
         producto.setTecnologia("satelital");
+        producto.setStatus(properties.getStatus());
 
         Productos producto1 = new Productos();
-        producto1.setCodProducto("PRO0002");
+        producto1.setCodProducto(properties.getPrefijcodigoProducto() + "0002");
         producto1.setNomProducto("INTERNET");
         producto1.setTecnologia("fibra optica");
+        producto1.setStatus(properties.getStatus());
 
         List<Productos> productos = new ArrayList<>();
         ;
@@ -41,7 +54,7 @@ public class ConsultarController {
         response.setFecha_Nacimiento("10/08/1997");
         response.setNumDocumento(request.getNumDocumento());
         response.setTipDocumento(request.getTipDocumento());
-        response.setCodCliente("C00021");
+        response.setCodCliente(properties.getPrefijCodigoCliente() + "00021");
 
         return response;
     }
@@ -54,9 +67,9 @@ public class ConsultarController {
 
         Integer cod = 12343;
 
-        response.setCodRespuesta("0000");
-        response.setMsgRespuesta("procesos exitoso");
-        response.setCodigoReclamo(request.getIdProducto() + cod.toString() + request.getNumDocumento());
+        response.setCodRespuesta(properties.getCodigoRepuesta());
+        response.setMsgRespuesta(properties.getMensajeRepuesta());
+        response.setCodigoReclamo(properties.getPreFijReclamo() + ": " + request.getIdProducto() + cod.toString() + request.getNumDocumento());
 
         return response;
     }
---
+++ src/main/java/ms/prueba/consultar/model/RequestService.java
@@ -0,0 +1,32 @@
+package ms.prueba.consultar.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class RequestService {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("tipDocumento")
+    private String tipDocumento;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("numDocumento")
+    private String numDocumento;
+
+    public String getTipDocumento() {
+        return tipDocumento;
+    }
+
+    public void setTipDocumento(String tipDocumento) {
+        this.tipDocumento = tipDocumento;
+    }
+
+    public String getNumDocumento() {
+        return numDocumento;
+    }
+
+    public void setNumDocumento(String numDocumento) {
+        this.numDocumento = numDocumento;
+    }
+
+}
